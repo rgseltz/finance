8fcb96f68525079401380f31a4a6ff79bf9debb2
ryan <ryanjgecelter@gmail.com>
2022-06-19 11:52:15 +0300

    imported g - flask global object for toggling session login to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session, flash
+from flask import Flask, render_template, redirect, session, flash, g
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, bcrypt, Portfolio, Stock, Portfolio_Stock, User_Stock
 from sqlalchemy.exc import IntegrityError
@@ -17,7 +17,22 @@ db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
+############Handle Session Data - User Login - Flask Global################
 
-@app.route('/')
+
+def add_global_user():
+    """If user is currently logged in, add user to flask global"""
+    if session['CURR_USER_KEY']:
+        g.user = User.query.get_or_404(session['CURR_USER_KEY'])
+
+
+################Flask Routes#################################
+
+@app.route('/', methods=["GET"])
 def show_homepage():
     return render_template('index.html')
+
+
+@app.route('/portfolios', methods=["GET"])
+def show_portfolios():
+    return render_template('portfolios.html')
